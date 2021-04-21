9a0de207694192a46ae77b62cfd3f3a23f2c43ee
Jonathan Aanesen <jonathan.aanesen@gmail.com>
2021-04-21 11:22:57 +0200

    Initial Commit

---
+++ Resting Heart Rate App WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Resting Heart Rate App WatchKit Extension
+//
+//  Created by Jonathan Aanesen on 21/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Resting Heart Rate App WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  Resting Heart Rate App WatchKit Extension
+//
+//  Created by Jonathan Aanesen on 21/04/2021.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
